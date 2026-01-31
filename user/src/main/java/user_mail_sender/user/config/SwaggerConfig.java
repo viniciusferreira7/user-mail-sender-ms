a9f9d15379a4cfc552373fef5d523f762fdef2bc
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-01-31 11:27:23 -0300

    config: add Swagger/OpenAPI configuration classes

---
+++ user/src/main/java/user_mail_sender/user/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package user_mail_sender.user.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI userServiceOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("User Service API")
+                        .description("Microservice for managing users and publishing email events via RabbitMQ")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("User Mail Sender Team")
+                                .email("contact@example.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0.html")));
+    }
+}
