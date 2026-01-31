50c8f14c6a271e7551f73f6190d1d187c75e9008
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-01-31 10:29:45 -0300

    build: add user service

---
+++ user/src/test/java/user_mail_sender/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package user_mail_sender.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
