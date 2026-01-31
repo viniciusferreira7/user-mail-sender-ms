102a15031651e5d2440e11aa7855b999f194ba8a
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-01-31 10:29:26 -0300

    build: add email service

---
+++ email/src/test/java/user_mail_sender/email/EmailApplicationTests.java
@@ -0,0 +1,13 @@
+package user_mail_sender.email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
