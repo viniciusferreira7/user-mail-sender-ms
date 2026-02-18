d7c5c15cc7f250f3db8ac3e2c6d1edd3dca0dc62
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-02-18 09:40:58 -0300

    feat(user): add createdAt and updatedAt fields

--- user/src/main/java/user_mail_sender/user/domain/UserModel.java
+++ user/src/main/java/user_mail_sender/user/domain/UserModel.java
@@ -4,7 +4,10 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @AllArgsConstructor
@@ -26,5 +29,13 @@ public class UserModel {
     @Column(nullable = false, unique = true)
     private String email;
 
-    
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+
 }
