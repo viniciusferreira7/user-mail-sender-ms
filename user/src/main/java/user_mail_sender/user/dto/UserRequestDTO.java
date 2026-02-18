b4f67c29ee94dba95bd97ea5d6c250ce53f6971d
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-02-18 09:41:40 -0300

    feat(user): add some annotations from validation lib into user request DTO

--- user/src/main/java/user_mail_sender/user/dto/UserRequestDTO.java
+++ user/src/main/java/user_mail_sender/user/dto/UserRequestDTO.java
@@ -1,9 +1,11 @@
 package user_mail_sender.user.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 
 @Builder
 public record UserRequestDTO (
-    String name,
-    String email
+    @NotBlank String name,
+    @NotBlank @Email String email
 ){}
