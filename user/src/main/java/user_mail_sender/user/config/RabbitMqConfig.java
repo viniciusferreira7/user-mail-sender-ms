4ccbee98280d3d2d3ab0d4ff20c718add2bf7adf
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-02-21 10:06:48 -0300

    feat(user): add rabbitmq configuration

---
+++ user/src/main/java/user_mail_sender/user/config/RabbitMqConfig.java
@@ -0,0 +1,51 @@
+package user_mail_sender.user.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMqConfig {
+    public static final String QUEEUE_NAME = "email-queue";
+    public static final String EXCHANGE_NAME = "email.exchange";
+    public static final String ROUTING_KEY = "email.routingKey";
+
+
+    @Bean
+    public Queue queue(){
+        return new Queue(QUEEUE_NAME, true);
+    }
+
+
+    @Bean
+    public DirectExchange exchange(){
+        return new DirectExchange(EXCHANGE_NAME);
+    }
+
+    @Bean
+    public Binding binding(){
+        return BindingBuilder
+                .bind(queue())
+                .to(exchange())
+                .with(ROUTING_KEY);
+    }
+
+    @Bean
+    public JacksonJsonMessageConverter messageConverter(){
+        return new JacksonJsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory){
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(messageConverter());
+        return template;
+    }
+}
