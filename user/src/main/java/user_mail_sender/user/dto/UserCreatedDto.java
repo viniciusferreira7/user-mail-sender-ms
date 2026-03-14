faf7789a2f770c4d04e9f6b9a8875d65e23fc862
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-03-14 09:12:30 -0300

    fix(user): fix validation annotations on user created dto

--- user/src/main/java/user_mail_sender/user/dto/UserCreatedDto.java
+++ user/src/main/java/user_mail_sender/user/dto/UserCreatedDto.java
@@ -1,5 +1,8 @@
 package user_mail_sender.user.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 import java.time.LocalDateTime;
@@ -7,8 +10,16 @@ import java.util.UUID;
 
 @Builder
 public record UserCreatedDto(
+        @NotNull
         UUID userId,
+
+        @NotBlank
         String name,
+
+        @NotBlank
+        @Email
         String email,
+
+        @NotNull
         LocalDateTime createdAt
 ) {}
